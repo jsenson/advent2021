14ae1bbcf7f55861494f1741d987ba89de994295
Jon Senson <jonsenson@gmail.com>
2021-12-07 13:16:38 -0500

    Day 7 solutions

---
+++ day07/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Shared;
+
+namespace day07 {
+	class Program {
+		static void Main(string[] args) {
+			int[] inputs = InputParser.ParseToInts(InputParser.INPUT, InputParser.COMMA);
+			Part1(inputs);
+			Console.WriteLine();
+			Part2(inputs);
+		}
+
+		static void Part1(int[] inputs) {
+			int result;
+			int[] copy = (int[])inputs.Clone();
+			Array.Sort(copy);
+			result = copy[copy.Length / 2];
+
+			Console.WriteLine("Part 1:");
+			Console.WriteLine($"Align to {result}: Costs {AlignTo(copy, result):n0} fuel.");
+		}
+
+		// Kinda cheated on this one and saw an answer on Reddit.  Answer is always aligned to mean +/- 0.5
+		static void Part2(int[] inputs) {
+			int total = 0;
+			int result = 0;
+			for(int i = 0; i < inputs.Length; i++) {
+				total += inputs[i];
+			}
+
+			float mean = total / (float)inputs.Length;
+			int mean1 = (int)(mean - 0.5f);
+			int mean2 = (int)(mean + 0.5f);
+			int cost1 = AlignTo(inputs, (int)(mean - 0.5f), 1);
+			int cost2 = AlignTo(inputs, (int)(mean + 0.5f), 1);
+			int finalCost;
+			if(cost1 < cost2) {
+				result = mean1;
+				finalCost = cost1;
+			} else {
+				result = mean2;
+				finalCost = cost2;
+			}
+
+			Console.WriteLine("Part 2:");
+			Console.WriteLine($"Align to {result}: Costs {finalCost:n0} fuel.");
+		}
+
+		static int AlignTo(int[] inputs, int pos, int costIncrease = 0) {
+			int total = 0;
+			for(int i = 0; i < inputs.Length; i++) {
+				int cost = Math.Abs(pos - inputs[i]);
+				if(costIncrease != 0) {
+					for(int j = 1, distance = cost; j <= distance; j++) {
+						cost += (j - 1) * costIncrease;
+					}
+				}
+
+				total += cost;
+			}
+
+			return total;
+		}
+	}
+}
---
+++ shared/InputParser.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+
+namespace Shared {
+	public static class InputParser {
+		public const string INPUT = "input.txt";
+		public const string DEBUG = "debuginput.txt";
+		public const string COMMA = ",";
+		public const string NEWLINE = "\n";
+
+		public static int[] ParseToInts(string fileName, string separator) {
+			return ParseToArray<int>(fileName, separator, entry => Convert.ToInt32(entry));
+		}
+
+		public static T[] ParseToArray<T>(string fileName, string separator, Func<string, T> parseFunc) {
+			string[] inputStrings = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), fileName)).Split(separator, StringSplitOptions.RemoveEmptyEntries);
+			T[] inputs = new T[inputStrings.Length];
+			for (int i = 0; i < inputStrings.Length; i++) {
+				inputs[i] = parseFunc(inputStrings[i]);
+			}
+
+			return inputs;
+		}
+	}
+}
