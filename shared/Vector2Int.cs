89a1c114ad256f97f2dc40ae0221e9d603fe8132
Jon Senson <jonsenson@gmail.com>
2021-12-13 16:55:04 -0500

    Added a Vector2Int for later use

---
+++ shared/Vector2Int.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Shared {
+	public struct Vector2Int {
+		public int x;
+		public int y;
+
+		public Vector2Int(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public Vector2Int(string valueString, string separator) {
+			string[] values = valueString.Split(separator, System.StringSplitOptions.RemoveEmptyEntries);
+			this.x = values.Length > 0 ? Convert.ToInt32(values[0]) : 0;
+			this.y = values.Length > 1 ? Convert.ToInt32(values[1]) : 0;
+		}
+
+		public Vector2Int Add(Vector2Int other) {
+			return new Vector2Int(this.x + other.x, this.y + other.y);
+		}
+
+		public static bool operator ==(Vector2Int a, Vector2Int b) {
+			return a.x == b.x && a.y == b.y;
+		}
+
+		public static bool operator !=(Vector2Int a, Vector2Int b) {
+			return !(a == b);
+		}
+
+		public override string ToString() {
+			return $"({x}, {y})";
+		}
+
+		public override bool Equals(object obj) {
+			if(obj is Vector2Int other) {
+				return this == other;
+			} else {
+				return object.ReferenceEquals(obj, this);
+			}
+		}
+
+		public override int GetHashCode() {
+			return x.GetHashCode() & y.GetHashCode();
+		}
+	}
+}
